4bf4e6bcd5a1dbbfe879cd5ae58e59445a54ca07
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-22 17:33:05 +0300

    complite 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+
+const makeGalleryStr = makeGallery(galleryItems).join('')
+
+function makeGallery(item){
+    return item.map(({preview, original, description}) =>{
+           return`<div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+             <img
+               class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+             />
+           </a>
+         </div>` 
+    })
+}
+
+galleryRef.innerHTML = makeGalleryStr
+galleryRef.addEventListener("click", (event)=>{
+    if(event.target.nodeName !== "IMG"){
+        return
+    }
+    event.preventDefault()
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`).show()
+})
